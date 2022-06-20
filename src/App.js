e428534ea4b9a033e6a664c8ffa0415185a35150
scarfacedotcom <peteradaaku@gmail.com>
2022-06-20 09:08:42 +0100

    some changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import './App.css';
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// page components
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <h1>My Articles</h1>
-      </nav>
+      <BrowserRouter>
+        <nav>
+          <h1>My Articles</h1>
+        </nav>
+
+        <Routes>
+          <Route path="/">
+            <Home />  
+          </Route>
+          <Route path="/about">
+            <About />  
+          </Route>
+          <Route path="/contact">
+            <Contact />  
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+export default App
