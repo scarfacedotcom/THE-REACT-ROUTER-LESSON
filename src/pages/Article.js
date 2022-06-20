98ff4089c98fd37bea998ed3f4ead5be7edb179f
scarfacedotcom <peteradaaku@gmail.com>
2022-06-20 22:33:58 +0100

    added the code for programattic redirect using the useEffect hook

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { useFetch } from "../hooks/useFetch"
 
@@ -6,6 +7,13 @@ export default function Article() {
   const { id } = useParams()
   const url = 'http://localhost:3000/articles/' + id
   const { data: article, isPending, error } = useFetch(url)
+
+  useEffect(() => {
+    if (error) {
+      // redirect
+      
+    }
+  },[error])
 
   return (
     <div>
